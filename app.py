b75e95d7f6cdded49bfc4fdbc0d0eed57d818749
mrtaalebi <the.doors.are.locked@gmail.com>
2020-01-18 16:35:52 +0330

    Initial Commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+import telegram
+from telegram.ext import Updater
+
+
+bot_token = os.getenv('BOT_TOKEN')
+
+updater = Updater(token=bot_token)
+dispatcher = updater.dispatcher
+
+import logging
+logging.basicConfig(
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+        level=logging.INFO
+)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
